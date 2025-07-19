4a4d4ee3aa13a0594982f0a5c229ba74332bfb6b
Yashi Verma <yashiverma323@gmail.com>
2025-07-19 20:55:26 +0530

    Added exercises

---
+++ Datatypes/chapter1.py
@@ -0,0 +1,8 @@
+sugar_amount=2
+print(f"Value of sugar amount is: {sugar_amount}")
+
+sugar_amount=12
+print(f"Second value of sugar is: {sugar_amount}")
+
+print(f" ID of 2: {id(2)}")
+print(f" ID of 12: {id(12)}")
---
+++ Datatypes/chapter2.py
@@ -0,0 +1,12 @@
+spice_mix=set()
+
+print(f" Initial spice mix is: {spice_mix}")
+print(f" Secondary spice mix is: {spice_mix}")
+
+spice_mix.add("Ginger")
+spice_mix.add("lemon")
+spice_mix.add("clove")
+spice_mix.add("cardamom")
+
+print(f" ID of spice mix: {id(spice_mix)}")
+print(f" Name of spices added: {spice_mix}")
