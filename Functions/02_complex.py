1288198e72fadc954fcae779f22b28cc2e6f0f78
yashi <yashi.verma@rahitechnologies.com>
2025-07-25 15:05:41 +0530

    Added code for functions

---
+++ Functions/02_complex.py
@@ -0,0 +1,22 @@
+def fetch_sales():
+    print("Fetching the sales data")
+    
+def filter_valid_sales():
+    print("Filtering the valid sales data")
+    
+def summarize_data():
+    print("Summarize sales data")
+    
+def generate_report():
+    fetch_sales()
+    filter_valid_sales()
+    summarize_data()
+    print("Report is Ready")
+    
+generate_report()
+
+# Output
+# Fetching the sales data
+# Filtering the valid sales data
+# Summarize sales data
+# Report is Ready
